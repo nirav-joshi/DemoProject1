41360c8f0be8f1fd42644eadfda9cec9a0fae040
niravj <niravjoshi.gdcst@gmail.com>
2018-05-22 12:06:59 +0530

    Initial Commit Frammwork

--- app/src/main/java/demosample/nirav/com/demoproject/MainActivity.java
+++ app/src/main/java/demosample/nirav/com/demoproject/MainActivity.java
@@ -1,13 +1,41 @@
 package demosample.nirav.com.demoproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+    TextView text1;
 
+    private static final String FORMAT = "%02d:%02d:%02d";
+
+    int seconds , minutes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text1=(TextView)findViewById(R.id.textview1);
+
+        new CountDownTimer(16069000, 1000) { // adjust the milli seconds here
+
+            public void onTick(long millisUntilFinished) {
+
+                text1.setText(""+String.format(FORMAT,
+                        TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
+                        TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(
+                                TimeUnit.MILLISECONDS.toHours(millisUntilFinished)),
+                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(
+                                TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+            }
+
+            public void onFinish() {
+                text1.setText("done!");
+            }
+        }.start();
+
     }
+
 }
