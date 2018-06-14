7ad153e6c6360132ad71e53bbd247803df4b158f
niravj <niravjoshi.gdcst@gmail.com>
2018-06-14 14:42:48 +0530

    Initial Commit Frammwork

---
+++ app/src/main/java/demosample/nirav/com/demoproject/Order/SchedulePickupActivity.java
@@ -0,0 +1,11 @@
+package demosample.nirav.com.demoproject.Order;
+
+import demosample.nirav.com.demoproject.R;
+import demosample.nirav.com.demoproject.base.AbstractBaseActivity;
+
+public class SchedulePickupActivity  extends AbstractBaseActivity {
+    @Override
+    protected int getContentView() {
+        return R.layout.pickup;
+    }
+}
